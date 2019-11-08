f1075190e259c7d3a61dd6bcdb592dfb7b496a03
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2019-11-08 17:11:14 -0600

    Uploaded back-end files

---
+++ READMECodeFiles/back-end/index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.post('/api/form', (req, res) => {
+  nodemailer.createTestAccount((err, account) => {
+    const htmlEmail = `
+      <h3>Contact Details</h3>
+      <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>
+        ${req.body.message}
+      </p>
+    `
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'exploringdatastructures@gmail.com',
+        pass: 'Cshatto0090'
+      }
+    })
+
+    let mailOptions = {
+      from: 'exploringdatastructures@gmail.com',
+      to: 'exploringdatastructures@gmail.com',
+      subject: 'New Contact Message',
+      text: req.body.message,
+      html: htmlEmail
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if(error) {
+        return console.log(error)
+      }
+
+      console.log('Message sent: %s', info.message)
+      console.log('Message URL %s', nodemailer.getTestMessageURL(info))
+
+    })
+  })
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server listening to port ${PORT}`)
+})
