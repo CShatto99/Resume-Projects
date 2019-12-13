2c2c5a80de5b964d06ac85bed2bb3eab89293437
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2019-12-12 21:58:29 -0600

    Uploaded a cloned project
    This project is a copy of the 21YearsOfGasPrices project that includes descriptions of all of the code.

---
+++ 21YearsOfGasPricesDescriptive/21YearsOfGasPrices/NewLine.h
@@ -0,0 +1,23 @@
+#pragma once
+
+using namespace std;
+
+class NewLine {
+public:
+
+	// declare the NewLine member variables
+	int index = 0, month = 0, day = 0, year = 0, size = 1065;
+	float price = 0;
+
+	// declare the default constructor
+	NewLine() {}
+
+	// set the member variables to the input values
+	void setValues(int month_, int day_, int year_, float price_) {
+		month = month_, day = day_, year = year_, price = price_;
+	}
+
+	// call the destructor
+	~NewLine() {}
+
+};
