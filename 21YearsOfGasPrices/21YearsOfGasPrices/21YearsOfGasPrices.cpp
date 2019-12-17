d0699bc470cdc5f1cda92569c1598ffaff0158b2
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2019-12-16 22:45:47 -0600

    Uploaded refactored files

--- 21YearsOfGasPrices/21YearsOfGasPrices/21YearsOfGasPrices.cpp
+++ 21YearsOfGasPrices/21YearsOfGasPrices/21YearsOfGasPrices.cpp
@@ -11,36 +11,52 @@ using namespace std;
 
 int main() {
 
+	// declare a fstream object and an array for every line in the input file
 	fstream inFile;
 	array<NewLine, 1065> lineInfo;
 	
+	// declare string variables to store each datapoint from the input file
 	string strYear, strPrice, strDay, strMonth;
+	// declare a size variable
 	int size = 0;
 
+	// open the GasPrices.txt file
 	inFile.open("GasPrices.txt", ios::in | ios::out);
 
-	if (inFile.fail())
+	// if the file failed to open
+	if (inFile.fail()) {
+
+		// display an error message
 		cout << "Error opening file.\n";
 
+	}
+
+	// if the input file is not closed
 	if (inFile.is_open()) {
 
+		// while none of the stream's error state flags have been set
 		while (inFile.good()) {
 
+			// get each datapoint and store them in their respective variables
 			getline(inFile, strMonth, '-');
 			getline(inFile, strDay, '-');
 			getline(inFile, strYear, ':');
 			getline(inFile, strPrice);
 
-			lineInfo[size++].setValues(stof(strMonth), stof(strDay), stof(strYear), stof(strPrice));
+			// set the values of the current lineInfo object using the integer conversions of the previous strings and increment the size
+			lineInfo[size++].setValues(stof(strMonth), stof(strDay), stof(strYear), stod(strPrice));
 
 		}
 
 	}
 
+	// close the input file
 	inFile.close();
 
+	// pass the array containing the input data to the NewLineMethods constructor
 	NewLineMethods execute(lineInfo);
 
+	// call each member function in the NewLineMethods class 
 	execute.avgYearlyPrice();
 	execute.avgMonthlyPrice();
 	execute.highYearlyPrice();
@@ -50,5 +66,4 @@ int main() {
 
 	system("PAUSE");
 	return 0;
-
 }
--- 21YearsOfGasPrices/21YearsOfGasPrices/NewLine.h
+++ 21YearsOfGasPrices/21YearsOfGasPrices/NewLine.h
@@ -4,15 +4,20 @@ using namespace std;
 
 class NewLine {
 public:
-	int index = 0, month = 0, day = 0, year = 0, size = 1065;
-	float price = 0;
 
+	// declare the NewLine member variables
+	int index = 0, month = 0, day = 0, year = 0, size = 1065;
+	double price = 0;
+
+	// declare the default constructor
 	NewLine() {}
 
-	void setValues(int month_, int day_, int year_, float price_) {
+	// set the member variables to the input values
+	void setValues(int month_, int day_, int year_, double price_) {
 		month = month_, day = day_, year = year_, price = price_;
 	}
 
+	// call the destructor
 	~NewLine() {}
 
 };
