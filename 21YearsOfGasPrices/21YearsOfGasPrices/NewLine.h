e10365956460bd36589628373394837d7a50e9a8
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2019-12-16 22:48:15 -0600

    Uploaded refactored files

--- 21YearsOfGasPrices/21YearsOfGasPrices/NewLine.h
+++ 21YearsOfGasPrices/21YearsOfGasPrices/NewLine.h
@@ -4,20 +4,15 @@ using namespace std;
 
 class NewLine {
 public:
-
-	// declare the NewLine member variables
 	int index = 0, month = 0, day = 0, year = 0, size = 1065;
 	double price = 0;
 
-	// declare the default constructor
 	NewLine() {}
 
-	// set the member variables to the input values
 	void setValues(int month_, int day_, int year_, double price_) {
 		month = month_, day = day_, year = year_, price = price_;
 	}
 
-	// call the destructor
 	~NewLine() {}
 
 };
